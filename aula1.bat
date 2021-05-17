90fc1eb6ff2cd0fc08d63316f387a58cb6488f79
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2021-05-17 19:44:28 -0300

    Create aula1.bat

---
+++ aula1.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set nome=valor
+set curso= Batch Script
+:: é a forma como se define variável em batch
+
+echo %curso%
+:: O comando echo é semelhante ao print de python
+:: serve para imprimir coisa na console
+:: no entanto devemos 
+pause > nul
