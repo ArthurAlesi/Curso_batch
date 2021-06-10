75bc72b90bafa171163768192213440a3fae1f4e
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2021-06-10 18:00:53 -0300

    Update aula5.bat

---
+++ aula5.bat
@@ -0,0 +1,15 @@
+::@echo off
+:: ERROR LEVEL
+:: o ENTRADA DE DADOS
+:: 1 SAIDA DE DADOS SEM ERRO
+:: 2 SAIDA DE DADOS COM ERRO
+:: 3 - 9 SAIDAS PERSONALIZADAS
+:: 10... CODIGOS DE ERRO
+
+set /a x=2
+set /a y=3
+
+goto 
+
+
+pause > nul
